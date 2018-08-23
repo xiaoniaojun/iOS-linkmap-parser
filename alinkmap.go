f70d3f6e45560caf3432a083631b36b384ea5849
Pecado <liuchang01@bilibili.com>
2018-08-23 15:02:55 +0800

    实现列出每个library的功能

--- alinkmap.go
+++ alinkmap.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
-	"bufio"
-	"strings"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 type LinkMap struct {
@@ -14,21 +16,24 @@ type LinkMap struct {
 }
 
 type Library struct {
-	Name string
+	Name    string
 	Objects []string
-	Indexes []uint
+	size    uint64
 }
 
 // 文件部位，用于控制读取方法
 const (
-	header = 1
-	objects = 2
-	segments = 3
+	header             = 1
+	objects            = 2
+	symbols            = 3
+	deadstripedsymbols = 4
+	segment            = 5
 )
 
 var linkmap LinkMap
 var parsePart = header
 var libraryList = make(map[string]*Library)
+var rowNum2LibMap = make(map[uint]*Library)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -50,68 +55,108 @@ func main() {
 	}
 
 	// TODO：参数控制打印信息
-	PrintLinkMap(linkmap)
-	PrintLibraryList(libraryList)
-	//--------
-	// test
-	//--------
-	println("=== main.o Indexes:")
-	for _, v := range libraryList["libWeChatSDK.a"].Objects {
-		println(v)
-	}
+	PrintLinkMapHeader(linkmap)
+	//PrintLibraryList(libraryList)
+
+	PrintLibSize()
 }
 
+func PrintLibSize() {
+	var totalSize uint64 = 0
+	var libSizePair = make(map[uint64]string)
+	var sizes []int
+	for libName, lib := range libraryList {
+		libSizePair[lib.size] = libName
+		sizes = append(sizes, int(lib.size))
+		totalSize += lib.size
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(sizes)))
+	fmt.Printf("TotalSize: %dkb\n", totalSize/1024)
+	fmt.Printf("%-50sSize\n", "Library Name")
+	for _, k := range sizes {
+		fmt.Printf("%-50s%dkb\n", libSizePair[uint64(k)], k/1024)
+		//println(lib_size_pair[uint64(k)], strconv.Itoa(k/1024)+"kb")
+	}
 
-
+}
 func PrintLibraryList(list map[string]*Library) {
 	for _, lib := range list {
-		println(lib.Name)
+		println("name:\t", lib.Name)
 	}
 }
 
 func parse(row string) {
 	switch parsePart {
 	case header:
-		ScanRow(row)
+		ParseHeader(row)
 	case objects:
-		if row == "# Symbols:" {
-			parsePart = segments
+		if row == "# Sections:" {
+			parsePart = segment
 			return
 		}
 		rowNum, valid := parseRowNumber(row)
 		libName, objectName := ParseObjectFileRow(row)
 		if objectName != "" {
 			if libraryList[libName] == nil && valid {
-				libraryList[libName] = &Library{libName, []string{objectName},    []uint {rowNum}}
+				libraryList[libName] = &Library{libName, []string{objectName}, 0}
 			} else {
 				libraryList[libName].Objects = append(libraryList[libName].Objects, objectName)
-				libraryList[libName].Indexes = append(libraryList[libName].Indexes, rowNum)
 			}
 		} else {
-			libraryList[libName] = &Library{libName, nil, []uint {rowNum}}
+			libraryList[libName] = &Library{libName, nil, 0}
 		}
+		rowNum2LibMap[rowNum] = libraryList[libName]
 
-	case segments:
+	case segment:
+		if row == "# Symbols:" {
+			parsePart = symbols
+			return
+		}
+	case symbols:
+		if strings.HasPrefix(row, "# Dead") {
+			parsePart = deadstripedsymbols
+			break
+		}
 		if strings.Contains(row, "# Address") {
 			break
 		}
 		ParseSymbolsRow(row)
+	case deadstripedsymbols:
+		break
 	}
+
 }
 
 func ParseSymbolsRow(row string) {
+	if len(row) <= 0 {
+		return
+	}
+	retSize, rowNum := parseSymbolsSizeAndRowNum(row)
+	lib := rowNum2LibMap[rowNum]
+	lib.size += retSize
+}
 
+func parseSymbolsSizeAndRowNum(row string) (retSize uint64, rowNum uint) {
+	num, _ := parseRowNumber(row)
+	l := strings.Index(row, "	")
+	r := strings.Index(row, "[")
+	sizeStr := strings.Trim(row[l:r], "	")
+	size, err := strconv.ParseInt(sizeStr, 0, 64)
+	if err != nil {
+		log.Fatal("解析size的时候出错")
+	}
+	return uint64(size), num
 }
 
 func ParseObjectFileRow(row string) (libName string, objectName string) {
 	//noinspection GoRedundantSecondIndexInSlices
-	libComponent := row[strings.LastIndex(row, "/") + 1:len(row)]
+	libComponent := row[strings.LastIndex(row, "/")+1 : len(row)]
 
 	leftBracketIdx := strings.Index(libComponent, "(")
 	rightBracketIdx := strings.LastIndex(libComponent, ")")
-	if leftBracketIdx != -1 && rightBracketIdx != -1 { 	//Bugly(libBugly.a-x86_64-master.o)
+	if leftBracketIdx != -1 && rightBracketIdx != -1 { //Bugly(libBugly.a-x86_64-master.o)
 		libName = libComponent[0:leftBracketIdx]
-		objectName = libComponent[leftBracketIdx + 1 : rightBracketIdx]
+		objectName = libComponent[leftBracketIdx+1 : rightBracketIdx]
 	} else { //BiliPadAppDelegate.o
 		libName = libComponent
 	}
@@ -126,14 +171,14 @@ func parseRowNumber(row string) (rowNum uint, valid bool) {
 		return 0, false
 	}
 	NumStr := strings.Trim(row[fstLeftBctIdx:fstRightBctIdx], "[] ")
-	rowNumber, err := strconv.Atoi(NumStr);
+	rowNumber, err := strconv.Atoi(NumStr)
 	if err != nil {
 		return 0, false
 	}
 	return uint(rowNumber), true
 }
 
-func ScanRow(row string) {
+func ParseHeader(row string) {
 	if strings.HasPrefix(row, "#") {
 		components := strings.Split(row, ":")
 		if len(components) != 2 {
@@ -145,7 +190,7 @@ func ScanRow(row string) {
 			linkmap.Path = strings.Trim(components[1], " ")
 		} else if prefix == "Arch" {
 			linkmap.Arch = strings.Trim(components[1], " ")
-		} else if prefix == "Object files"{
+		} else if prefix == "Object files" {
 			parsePart = objects
 		} else {
 			log.Fatal("我们不该见面的")
@@ -153,8 +198,9 @@ func ScanRow(row string) {
 	}
 }
 
-func PrintLinkMap(linkmap LinkMap) {
-	println("================== LinkMap ==================")
-	println("Path: " + linkmap.Path)
-	println("Arch: " + linkmap.Arch)
+func PrintLinkMapHeader(linkmap LinkMap) {
+	fmt.Println("================== LinkMap ==================")
+	fmt.Println("Path: " + linkmap.Path)
+	fmt.Println("Arch: " + linkmap.Arch)
+	fmt.Println("")
 }
